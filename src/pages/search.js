c6c2aa213f564cf583b4ed6f995ac2015a0eacbe
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2021-12-31 19:15:37 +0500

    subaru done search bar

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,15 +1,35 @@
+import axios from "axios";
 import React from "react";
 
 class searchbar extends React.Component {
+  
+  state = {
+    name: ""
+  }
+
+  submit = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post("http://localost:8080/123", this.state);
+    console.log(res.data);
+  }  
+
+  change(e) {
+    console.log(e)
+    this.setState({
+      name: e.target.value
+    });
+  }
+
   render() {
     return (
-        <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <div class="login-container">
+        <div className="topnav">
+        <a className="active" href="#home">Home</a>
+        <div className="login-container">
           <nav>
-          <form method="POST">
-            <input type="text" placeholder="Search" name="name"/>
-            <button type="submit">Search</button>
+          <form method="POST" onSubmit={this.submit}>
+            <input type="text" placeholder="Search" name="name" onChange={this.change}/>
+            <button type="submit" >Search</button>
           </form>
           </nav>
         </div>
